ee2bbdb8612a65d0cf2eb88b75c517901e37d2f1
frantecbh <franciscomenezesbh@gmail.com>
2022-04-14 13:52:42 -0300

    foreach

--- server.js
+++ server.js
@@ -6,7 +6,40 @@ const app = express()
 app.set("view engine", "ejs")
 
 app.get("/", (request, response) => {
-    response.render("pages/index")
+
+    const items = [
+        {
+            title: "D",
+            message: "esenvolver aplicações/serviços de forma fácil"
+        },
+        {
+            title: "E",
+            message: "JS usa JavaScript para renderizar HTML"
+        },
+        {
+            title: "M",
+            message: "uito fácil de usar"
+        },
+        {
+            title: "A",
+            message: "gil"
+        },
+        {
+            title: "I",
+            message: "nstall ejs"
+        },
+        {
+            title: "S",
+            message: "intaxe simples"
+        },
+    ]
+
+    const subtitle = "Uma linguagem de modelagem para criação de página HTML utilizando EJS "
+
+    response.render("pages/index", {
+        qualitys: items,
+        subtitle: subtitle
+    })
 })
 
 app.get("/sobre", (request, response) => {
